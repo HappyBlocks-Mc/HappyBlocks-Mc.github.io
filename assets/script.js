60751ef0a6123cf1e3e7139f52e4f779547e0216
Pizzacus <pizzacus@gmail.com>
2017-05-01 16:29:39 +0200

    Iframe redirection (eww...) + Cool background
    And better buttons + improved google speedinsight ranking a bit...
    probably...
    
    Yeah, I know this "iframe redirection" thing is horrible, don't judge
    me, judge @StoneSet, he was the one who REALLY wanted this

--- assets/script.js
+++ assets/script.js
@@ -14,3 +14,53 @@ function changeSlide(slideshowID,slideName) {
 	if (typeof selectedButton !== null)
 	selectedButton.className = selectedButton.className.replace(/(\sselected|selected\s)/, "");
 }
+
+if (Clipboard.isSupported()) {
+	var button = document.getElementById("nav-copy-button");
+	button.className = "button";
+	button.setAttribute("title", "Cliquez pour copier");
+
+	var popupIsOnScreen = false;
+
+	var clipboard = new Clipboard(button);
+	clipboard.on("success",  function () {
+		if (!popupIsOnScreen) {
+			popupIsOnScreen = true;
+
+			document.getElementById("copy-text").className = "visible";
+
+			window.setTimeout(function () {
+				// Amazing and incomprehensible trick to make the css animation play again
+				document.getElementById("copy-text").className = ""
+				void document.getElementById("copy-text").offsetWidth;
+				document.getElementById("copy-text").className = "visible hiding";
+			}, 3000);
+
+			window.setTimeout(function () {
+				document.getElementById("copy-text").className = "";
+				popupIsOnScreen = false;
+			}, 3400);
+		}
+	})
+}
+
+var x = 0,
+y = 0;
+
+backgroundContainer = document.getElementById("background");
+
+document.body.onmousemove = function (e) {
+	x = event.clientX;
+	y = event.clientY;
+}
+
+function updateBackground() {
+	var translateX = -(x - (window.innerWidth / 2)) / window.innerWidth * 15 + "px",
+	translateY = -(y - (window.innerHeight / 2)) / window.innerHeight * 15 + "px";
+
+	backgroundContainer.style.transform = "translate(" + translateX + ", " + translateY + ")" // Translate is the least laggy way to do thig thing
+
+	window.requestAnimationFrame(updateBackground);
+};
+
+window.requestAnimationFrame(updateBackground);
---
+++ iframes/test.md
@@ -0,0 +1,7 @@
+---
+layout: iframe
+title: test
+permalink: /test/
+
+iframe_url: "https://steamcommunity.com/sharedfiles/filedetails/?id=405099188" 
+---
